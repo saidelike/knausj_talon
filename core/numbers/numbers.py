5d9e9bae067c852381e4a7fbbe151ecfb78aa0d9
Cedric Halbronn <cedric.halbronn@nccgroup.com>
2023-09-05 13:57:56 +0100

    only allow 'numb 1 2 3' instead of '1 2 3' directly in command mode to avoid false positives

--- core/numbers/numbers.py
+++ core/numbers/numbers.py
@@ -220,3 +220,18 @@ def number_signed_small(m) -> int:
     """Parses an integer between -99 and 99."""
     number = m[-1]
     return -number if (m[0] in ["negative", "minus"]) else number
+
+
+@mod.capture(rule="<user.number_string> point <user.number_string>")
+def number_float_string(m) -> str:
+    """Parses a float number phrase, returning that number as a string."""
+    return f"{m.number_string_1}.{m.number_string_2}"
+
+
+@mod.capture(rule="(numb | number) (<user.number_string> | <user.number_float_string>)")
+def number_prefix(m) -> str:
+    """Parses a prefixed number phrase, returning that number as a string."""
+    try:
+        return m.number_string
+    except AttributeError:
+        return m.number_float_string
--- core/numbers/numbers.talon
+++ core/numbers/numbers.talon
@@ -1,3 +1,6 @@
 not tag: user.mouse_grid_showing
 -
-<user.number_string>: "{number_string}"
+# disable numbers without saying "numb" in front to avoid false positives
+# <user.number_string>: "{number_string}"
+# and only support something like "numb fifty five point two" -> 55.2
+<user.number_prefix>:       "{number_prefix}"
